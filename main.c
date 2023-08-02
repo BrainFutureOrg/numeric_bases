e58749ab6f2f07238341904d827001226278c548
KosenkoAlexander <alexandkosenko@gmail.com>
2023-08-02 17:07:46 +0300

    Changes by Oleksandr - digit to sequence usage

--- main.c
+++ main.c
@@ -3,5 +3,19 @@
 
 int main()
 {
+    int number, base;
 
+    printf("Enter a number in the decimal number system: ");
+    scanf("%d", &number);
+
+    printf("Enter the base of the number system (for example, 2, 8, 16): ");
+    scanf("%d", &base);
+
+    digit_sequence sequence= number_to_sequence(number, base);
+    print_digit_sequence(sequence);
+
+    free_digit_sequence(sequence);
+
+    return 0;
 }
+
